a179ea28c06df47fb624a515e75a1d98b6d57630
friesele <ethanfriesel@gmail.com>
2023-08-30 12:04:41 -0400

    First Changes and Button learning - in class 8/30/23

---
+++ src/main/java/com/example/githubgame/HelloController.java
@@ -0,0 +1,29 @@
+package com.example.githubgame;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private Button myButton;
+    @FXML
+    private Button buttonA;
+    @FXML
+    private Button buttonB;
+
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void buttonClicked(ActionEvent itemClicked){
+        Button buttonClicked = (Button) itemClicked.getSource();
+        welcomeText.setText(buttonClicked.getText());
+    }
+}
